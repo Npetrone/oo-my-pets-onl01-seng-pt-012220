f058492ab664b501d18ffc5bcfb51908becf6416
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 19:32:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+   attr_accessor :name, :owner
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
 end
