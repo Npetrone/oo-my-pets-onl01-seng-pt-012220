00dda38fe43e8f278a09dd31a2ff236b0fe5e78d
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 19:32:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+ attr_accessor :name, :owner
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ 
 end
